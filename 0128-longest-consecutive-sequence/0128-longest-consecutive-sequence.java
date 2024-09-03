48b067f0640a14a6d5b5ba4208ac591d5816f8d1
kamja0510 <89302303+kamja0510@users.noreply.github.com>
2024-09-03 14:11:04 +0900

    Time: 27 ms (69.56%), Space: 66.2 MB (49.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -15,23 +15,34 @@ class Solution {
         // answer = Math.max(nums[nums.length-1] + 1 - start, answer);
         // return answer;
 
-        if(nums.length ==0) return 0;
-        int answer = 1, count = 1;
-        Map<Integer, Integer> hashTable = new HashMap<>();
-        for(int num : nums){
-            hashTable.put(num, 1);
-        }
-        for(int num: nums){
-            if(!hashTable.containsKey(num-1)){
-                count = 1;
-                while(hashTable.containsKey(num+1)){
-                    num++;
-                    count++;
-                }
-                answer = Math.max(answer, count);
-            }
-        }
-        return answer;
+        // if(nums.length ==0) return 0;
+        // int answer = 1, count = 1;
+        // Map<Integer, Integer> hashTable = new HashMap<>();
+        // for(int num : nums){
+        //     hashTable.put(num, 1);
+        // }
+        // for(int num: nums){
+        //     if(!hashTable.containsKey(num-1)){
+        //         count = 1;
+        //         while(hashTable.containsKey(num+1)){
+        //             num++;
+        //             count++;
+        //         }
+        //         answer = Math.max(answer, count);
+        //     }
+        // }
+        // return answer;
 
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int maxCnt = 0;
+        for (int num : set) {
+            if(set.contains(num-1)) continue;
+            int cnt =0;
+            while(set.contains(num++)) cnt++;
+            maxCnt = Math.max(maxCnt,cnt);
+        }
+        return maxCnt;
     }
 }
